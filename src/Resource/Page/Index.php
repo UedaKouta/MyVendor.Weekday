0d3bbf23f15fca07793c4d8f41d065ea90d9bc71
kotaueda <k.u.okome3214@gmail.com>
2020-06-03 15:05:56 +0900

    modified index.php

--- src/Resource/Page/Index.php
+++ src/Resource/Page/Index.php
@@ -1,16 +1,39 @@
 <?php
 namespace MyVendor\Weekday\Resource\Page;
 
+//use BEAR\Resource\Annotation\Embed;
 use BEAR\Resource\ResourceObject;
+use BEAR\Sunday\Inject\ResourceInject;
 
 class Index extends ResourceObject
 {
-    public function onGet(string $name = 'BEAR.Sunday') : ResourceObject
-    {
-        $this->body = [
-            'greeting' => 'Hello ' . $name
-        ];
 
-        return $this;
-    }
+//@Embedでリソースを埋め込むかわりにuse ResourceInject;でresourceリソースクライアントを
+//インジェクトしてそのクライアントでappリソースをセットすることもできる
+
+   // /**
+   //   * @Embed(rel="weekday", src="app://self/weekday{?year,month,day}")
+   //   */
+   //  public function onGet(int $year, int $month, int $day) : ResourceObject
+   //  {
+   //      $this->body += [
+   //          'year' => $year,
+   //          'month' => $month,
+   //          'day' => $day
+   //      ];
+   //
+   //      return $this;
+   //  }
+
+use ResourceInject;
+
+public function onGet(int $year, int $month, int $day) : ResourceObject
+{
+  $params = get_defined_vars(); // ['year' => $year, 'month' => $month, 'day' => $day]
+  $this->body = $params + [
+      'weekday' => $this->resource->get('app://self/weekday', $params)
+  ];
+
+  return $this;
+}
 }
